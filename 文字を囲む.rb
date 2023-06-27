98b23bfe3ab4b52b260e0e3bef8d517e14ded634
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-06-27 04:48:00 +0000

    復習2

--- 文字を囲む.rb
+++ 文字を囲む.rb
@@ -1,11 +1,7 @@
-def frame(text)
-line = "+"*(text.length+2)
-
-puts line
-puts "+#{text}+"
-puts line
-
-end
-
 s = gets.chomp
-puts frame(s)
+
+line = "+"*(s.length+2)
+
+puts "+#{line}"
+puts "+#{s}+"
+puts "+#{line}"
